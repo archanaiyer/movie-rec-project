0e34c07f8c10db56d25eb7b16bd73bcd0f5ecd70
Archana Iyer <archanaiyer92@gmail.com>
2016-05-05 22:23:50 -0400

    added new service

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -318,15 +318,12 @@ app.controller('RatingCtrl', ['$scope', 'auth', 'movies',
 
 app.service('gtm', function ($rootScope, $window) {
     angular.element(document).ready(function () {
-        (function (w, d, s, l, i) {
-            w[l] = w[l] || []; w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
-            var f = d.getElementsByTagName(s)[0],
-                j = d.createElement(s),
-                dl = l != 'dataLayer' ? '&l=' + l : '';
-            j.async = true;
-            j.src = '//www.googletagmanager.com/gtm.js?id=' + i + dl;
-            f.parentNode.insertBefore(j, f);
-        })($window, document, 'script', 'tm', 'GTM-PQCVND');
+				(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({
+				'gtm.start':new Date().getTime(),event:'gtm.js'});
+				var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+				j.async=true;j.src='//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+				// })(window,document,'script','dataLayer','GTM-5Q98JN');
+				})(window,document,'script','dataLayer','GTM-PQCVND');
         //note: I've changed original code to use $window instead of window
     });
 
